74caf3392b0c1794dc44de4450d82c941b151361
Guides et support de cours <61227430+rougecitron@users.noreply.github.com>
2020-06-05 16:06:27 +0200

    maj suite cours

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,17 +10,12 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
         $client = new Client();
         $client->setSociete('Jokes Cie');
         $client->setNom('GOLADE');
         $client->setPrenom('Larry');
         $client->setEmail('larry.golade@jokescie.com');           
         $manager->persist($client);
-        $manager->flush();
-
         $manager->flush();
     }
 }
